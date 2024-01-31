f98fcbffa02a96ac7aaf02c461bf388c58723c49
Marko Nikolic <marko.supergun@gmail.com>
2024-01-31 18:30:07 +0100

    first commit

---
+++ src/config.js
@@ -0,0 +1,15 @@
+export const DEFAULT_BROWSERS = ["last 4 version"];
+export const CSS_EXAMPLE = `.example {
+    display: grid;
+    transition: all .5s;
+    user-select: none;
+    background: linear-gradient(to bottom, white, black);
+} 
+
+/* 
+ * 
+ * For Help: 
+ * https://portfolio.eronelit.com/?p=projects&flood=help
+ * 
+ */
+`
